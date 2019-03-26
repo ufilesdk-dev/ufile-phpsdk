bf2cc68dabddf161b7c56e8f2d973c5f90249689
delexxie <39333450+delexxie@users.noreply.github.com>
2019-03-26 16:57:10 +0800

    Update conf.php
    修改ufile.ucloud.cn为ufileos.com

--- v1/ucloud/conf.php
+++ v1/ucloud/conf.php
@@ -8,7 +8,8 @@ global $UCLOUD_PRIVATE_KEY;
 $SDK_VER = "1.0.8";
 
 //空间域名后缀,请查看控制台上空间域名再配置此处
-$UCLOUD_PROXY_SUFFIX = '.ufile.ucloud.cn';
+//https://docs.ucloud.cn/storage_cdn/ufile/tools/introduction
+$UCLOUD_PROXY_SUFFIX = '.ufileos.com';    
 
 $UCLOUD_PUBLIC_KEY = 'paste your public key here';
 $UCLOUD_PRIVATE_KEY = 'paste your private key here';
