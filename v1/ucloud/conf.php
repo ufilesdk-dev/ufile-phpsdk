468120b911bce4e18d28b5da84bca5e0f0adb3fc
delexxie <39333450+delexxie@users.noreply.github.com>
2019-03-26 16:59:15 +0800

    Update conf.php

--- v1/ucloud/conf.php
+++ v1/ucloud/conf.php
@@ -9,7 +9,7 @@ $SDK_VER = "1.0.8";
 
 //空间域名后缀,请查看控制台上空间域名再配置此处
 //https://docs.ucloud.cn/storage_cdn/ufile/tools/introduction
-$UCLOUD_PROXY_SUFFIX = '.ufileos.com';    
+$UCLOUD_PROXY_SUFFIX = '.cn-bj.ufileos.com';      //如果是其他地域的，请参考上面的说明
 
 $UCLOUD_PUBLIC_KEY = 'paste your public key here';
 $UCLOUD_PRIVATE_KEY = 'paste your private key here';
